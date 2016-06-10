63fb1493ea48db21506c305d0c968999f9347bc9
Koward <kiwyh723@gmail.com>
2016-06-10 14:07:51 +0200

    Fixed "-h 80" that was limit height in scroll list.

--- M2Export/M2EditorNode.cs
+++ M2Export/M2EditorNode.cs
@@ -11,6 +11,14 @@ namespace M2Export
         public static MObject AnimClipName;
         public static MObject AnimClipStart;
         public static MObject AnimClipEnd;
+
+        //TODO type enum
+        public static MObject LoopingFlag;
+        public static MObject LowPriorityFlag;
+        public static MObject Repetitions;
+        public static MObject BlendingFlag;
+        public static MObject BlendTime;
+        public static MObject Probability;
         public static MObject ExportAnimClip;
 
         public static MObject AnimClips;
@@ -25,19 +33,52 @@ namespace M2Export
             // Animation clips
             AnimClipName = tAttr.create("animClipName", "acn", MFnData.Type.kString);
             AnimClipStart = nAttr.create("animClipStart", "acs", MFnNumericData.Type.kInt);
+            nAttr.setMin(0);
             AnimClipEnd = nAttr.create("animClipEnd", "ace", MFnNumericData.Type.kInt);
-            ExportAnimClip = nAttr.create("exportAnimClip", "eac", MFnNumericData.Type.kBoolean, 1);
+            nAttr.setMin(0);
+            LoopingFlag = nAttr.create("animClipLooping", "acl", MFnNumericData.Type.kBoolean);
+            nAttr.setDefault(true);
+            LowPriorityFlag = nAttr.create("animClipLowPriority", "aclp", MFnNumericData.Type.kBoolean);
+            nAttr.setDefault(true);
+            Repetitions = nAttr.create("animClipRep", "acRep", MFnNumericData.Type.k2Float);
+            nAttr.setDefault((float) 0, 0);
+            nAttr.setMin(0, 0);
+            BlendingFlag = nAttr.create("animClipBlending", "acb", MFnNumericData.Type.kBoolean);
+            nAttr.setDefault(true);
+            BlendTime = nAttr.create("animClipBlendTime", "acbt", MFnNumericData.Type.k2Int);
+            nAttr.setDefault(150, 150);
+            nAttr.setMin(0, 0);
+            Probability = nAttr.create("animClipProbability", "acp", MFnNumericData.Type.kFloat);
+            nAttr.setDefault((float) 100);
+            nAttr.setMin(0);
+            nAttr.setMax(100);
+            ExportAnimClip = nAttr.create("exportAnimClip", "eac", MFnNumericData.Type.kBoolean);
+            nAttr.setDefault(true);
+
             addAttribute(AnimClipName);
             addAttribute(AnimClipStart);
             addAttribute(AnimClipEnd);
+            addAttribute(LoopingFlag);
+            addAttribute(LowPriorityFlag);
+            addAttribute(Repetitions);
+            addAttribute(BlendingFlag);
+            addAttribute(BlendTime);
+            addAttribute(Probability);
             addAttribute(ExportAnimClip);
 
             AnimClips = cAttr.create("animClips", "clips");
             cAttr.addChild(AnimClipName);
             cAttr.addChild(AnimClipStart);
             cAttr.addChild(AnimClipEnd);
+            cAttr.addChild(LoopingFlag);
+            cAttr.addChild(LowPriorityFlag);
+            cAttr.addChild(Repetitions);
+            cAttr.addChild(BlendingFlag);
+            cAttr.addChild(BlendTime);
+            cAttr.addChild(Probability);
             cAttr.addChild(ExportAnimClip);
             cAttr.isArray = true;
+
             addAttribute(AnimClips);
 
             return true;
--- README.md
+++ README.md
@@ -3,15 +3,19 @@
 ***Work in Progress, plugin not available yet***
 
 ## How to use this plugin ?
-You have 3 files, M2Export.nll.dll, M2Lib.dll and M2ExportUIOptions.mel.
+You have 2 folders in the zip, "plugins" and "scripts".
 
-1) Put M2Export.nll.dll and M2Lib.dll in C:\Program Files\Autodesk\Maya2016\bin\plugins
-2) Put M2ExportUIOptions.mel in C:\Users\<Your name>\Documents\maya\scripts
+1) Put "plugins" in "C:\Program Files\Autodesk\Maya2016\bin\"
+2) Put "scripts" in "C:\Users\<Your name>\Documents\maya\"
 3) Open Maya, go in the top menu Windows > Settings/Preferences > Plug-in Manager
 4) Click "Browse" and find M2Export.nll.dll
 5) In the list, next to M2Export.nll.dll, check "Loaded" and "Auto load"
 
-Now, to use it,
+You can prepare your models using the tools available in the top menu "WoW Tools".
+
+Now, to export a model,
 1) Go in the top menu File > Export All and select "World of Warcraft M2" as the export type.
-2) Select the expansion you want in the right menu (at the moment default is Lich King)
+2) Select the expansion you want in the right menu (default is Wrath of the Lich King)
 3) Type the file name, like "Frog.m2" and export.
+
+Special thanks to Vellear, who provided many sample models and taught me some Maya basics.
